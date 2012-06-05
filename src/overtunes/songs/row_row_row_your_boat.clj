d30790c098d419a08ad1b98320a7b2de050aa09a
Chris Ford <christophertford@gmail.com>
2012-06-05 20:54:42 +0100

    Simplified the piece to better serve as an example to newcomers.

--- src/overtunes/songs/row_row_row_your_boat.clj
+++ src/overtunes/songs/row_row_row_your_boat.clj
@@ -1,34 +1,39 @@
 (ns overtunes.songs.row-row-row-your-boat
   (:use
-    [overtone.live :only [at now]]
-    [overtone.inst.sampled-piano :only [sampled-piano] :rename {sampled-piano piano}]))
+    [overtone.live]))
 
-(defn sum-n [series n] (reduce + (take n series)))
-(def major-scale (partial sum-n (cycle [2 2 1 2 2 2 1])))
-(def g-major #(-> % major-scale (+ 67))) 
+(definst harpsichord [freq 440]
+  (let [duration 1]
+    (*
+      (line:kr 1 1 duration FREE)
+      (pluck (* (white-noise) (env-gen (perc 0.001 5) :action FREE))
+             1 1 (/ 1 freq) (* duration 2) 0.25))))
 
-(defn bpm [per-minute] #(-> % (/ per-minute) (* 60) (* 1000)))
-(defn after [timing beats] #(-> % (+ beats) timing))
-(defn syncopate [timing durations] #(->> % (sum-n durations) timing))
-(defn from [timing start] #(-> % timing (+ start))) 
+(def notes
+  (let [pitches
+         [67 67 67 69 71              ; Row, row, row your boat,
+          71 69 71 72 74              ; Gently down the stream,
+          79 79 79 74 74 74 71 71 71  ; Merrily, merrily, merrily, merrily,
+          67 67 67 74 72 71 69 67]    ; Life is but a dream! 
+        durations
+         [1 1 2/3 1/3 1
+          2/3 1/3 2/3 1/3 2
+          1/3 1/3 1/3 1/3 1/3 1/3 1/3 1/3 1/3 1/3 1/3 1/3
+          2/3 1/3 2/3 1/3 2]
+        times (reductions + 0 durations)]
+    (map vector timings pitches)))
 
-(def pitches [0 0 0 1 2, 2 1 2 3 4, 7 7 7 4 4 4 2 2 2 0 0 0, 4 3 2 1 0])
-(def durations [1 1 2/3 1/3 1, 2/3 1/3 2/3 1/3 2, 1/3 1/3 1/3 1/3 1/3 1/3 1/3 1/3 1/3 1/3 1/3 1/3, 2/3 1/3 2/3 1/3 2])
 
-(defn melody# [timing notes] 
-  (let [note# #(at (timing %1) (piano (g-major %2)))]
-    (dorun (map-indexed note# notes)))) 
+(defn play [metro notes] 
+  (let [play-note (fn [[beat midi]] (at (metro beat) (-> midi midi->hz harpsichord)))]
+    (dorun (map play-note notes)))) 
 
-(defn play# []
-  (let [timing (-> (bpm 150) (from (now)))
-        rhythm-from #(syncopate (after timing %) durations)]
-    (melody# (rhythm-from 0)  pitches)
-    (melody# (rhythm-from 16) pitches)
-    (melody# (rhythm-from 20) pitches)
-    (melody# (rhythm-from 24) pitches)
-    (melody# (rhythm-from 28) pitches)
-    ))
+(defn play-round [metro notes]
+  (let [after (fn [beats metro] (comp metro #(+ % beats)))]
+    (play metro notes)
+    (play (after 4 metro) notes)
+    (play (after 8 metro) notes)
+    (play (after 16 metro) notes)))
 
-(play#)
-
-(piano 77) 
+;(play (metronome 120) notes)
+;(play-round (metronome 120) notes)
