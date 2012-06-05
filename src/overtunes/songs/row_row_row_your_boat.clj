f7ef2190a7c4a8e4ac46603f3f364547a63abc5d
Chris Ford <christophertford@gmail.com>
2012-06-05 21:44:49 +0100

    Use keywords rather than midi.

--- src/overtunes/songs/row_row_row_your_boat.clj
+++ src/overtunes/songs/row_row_row_your_boat.clj
@@ -9,23 +9,29 @@
       (pluck (* (white-noise) (env-gen (perc 0.001 5) :action FREE))
              1 1 (/ 1 freq) (* duration 2) 0.25))))
 
-(def notes
+(def melody 
   (let [pitches
-         [67 67 67 69 71              ; Row, row, row your boat,
-          71 69 71 72 74              ; Gently down the stream,
-          79 79 79 74 74 74 71 71 71  ; Merrily, merrily, merrily, merrily,
-          67 67 67 74 72 71 69 67]    ; Life is but a dream! 
+         [:C4 :C4 :C4 :D4 :E4         
+          ; Row, row, row your boat,
+          :E4 :D4 :E4 :F4 :G4
+          ; Gently down the stream,
+          :C5 :C5 :C5 :G4 :G4 :G4 :E4 :E4 :E4 :C4 :C4 :C4 
+          ; Merrily, merrily, merrily, merrily,
+          :G4 :F4 :E4 :D4 :C4]
+          ; Life is but a dream! 
         durations
          [1 1 2/3 1/3 1
           2/3 1/3 2/3 1/3 2
           1/3 1/3 1/3 1/3 1/3 1/3 1/3 1/3 1/3 1/3 1/3 1/3
           2/3 1/3 2/3 1/3 2]
         times (reductions + 0 durations)]
-    (map vector timings pitches)))
+    (map vector times pitches)))
+
+(note :C3)
 
 
 (defn play [metro notes] 
-  (let [play-note (fn [[beat midi]] (at (metro beat) (-> midi midi->hz harpsichord)))]
+  (let [play-note (fn [[beat pitch]] (at (metro beat) (-> pitch note midi->hz harpsichord)))]
     (dorun (map play-note notes)))) 
 
 (defn play-round [metro notes]
@@ -35,5 +41,5 @@
     (play (after 8 metro) notes)
     (play (after 16 metro) notes)))
 
-;(play (metronome 120) notes)
-;(play-round (metronome 120) notes)
+;(play (metronome 120) melody)
+;(play-round (metronome 120) melody)
